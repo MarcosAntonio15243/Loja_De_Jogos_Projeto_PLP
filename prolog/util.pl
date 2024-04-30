3a08dc523b332a3b6ba586013137f96ef91306a4
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2024-04-29 21:06:33 -0300

    Adaptando função de limpar tela para sistemas Windows

--- prolog/util.pl
+++ prolog/util.pl
@@ -6,17 +6,18 @@
 ]).
 :- use_module(library(process)).
 
-/* Função para limpar o terminal utilizando o comando adequado de acordo com o sistema opracional (Linux ou Windows) */
+/* Função para limpar o terminal utilizando o comando adequado de acordo com o sistema opracional (Windows, Linux ou outro) */
 limparTela :-
     current_prolog_flag(windows, true), % Ainda sem funcionar corretamente
-    shell('powershell Clear-Host'), !.
-    %process_create(path('ghci'), ['limparTela.hs', '-e', 'main', '-e', ':quit'], []), !.
-    %process_create(path('cmd.exe'), ['/c', 'cls'], []), !.
+    % Cria o processo para limpar a tela
+    process_create(path(cmd), ['/C', 'cls'], [process(PID)]),
+    % Aguarda o término do processo
+    process_wait(PID, _), !.
 limparTela :-
     current_prolog_flag(unix, true),
     shell('clear'), !.
 limparTela :-
-    write('\33\[2J'), !.
+    write('\33\[2J').
 
 
 /* Recebe uma lista de parâmetros String e verifica se algum deles é vazio */
